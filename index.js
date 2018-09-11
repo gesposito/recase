e909c283fd5e5e5165dfd263f5f7ac50dc25a759
Gianluca Esposito <g@esposi.to>
2018-09-11 17:53:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class Index extends Component {
+  render() {
+    return <section>Recase</section>;
+  }
+}
+
+render(<Index />, document.getElementById("app"));
